b363bc99889cbd90d9203b11b781f1e6df80145d
MrSigma123 <karol.czolpinski@gmail.com>
2024-05-02 23:53:27 +0200

    Add project design overview

---
+++ chess.c
@@ -0,0 +1,16 @@
+/* chess.c console version of chess
+  GOAL: Fully functional chess for two players or very easy CPU opponent
+  (level 0) with reandom moves
+
+  TODO:
+
+  - implement all pieces moves
+  - castling (probably would be most challenging)
+  - pawn promotion
+  - en passant
+  - consider move highlighting
+
+  - design the chessboard representation
+
+  - for future releases - expand the program core with GUI with different
+    languages and packages*/
